a29b86b09cb7ca8fee40687598ba3181d4fea302
Christian Imfeld <christian.imfeld@siemens.com>
2023-09-15 10:51:49 +0200

    save check-in

--- ExtractLogFileConfig.ps1
+++ ExtractLogFileConfig.ps1
@@ -1,15 +1,31 @@
-[xml]$config = Get-Content "C:\Program Files\Siemens\LMS\bin\lmslogcfg.xml"    
-$appender = $config.configuration.appender | Where-Object {$_.name -eq "ALL-OUT"}    
-$fileValue = $appender.param | Where-Object {$_.name -eq "File"} | Select-Object -ExpandProperty value    
-$maxBackupIndex = $appender.param | Where-Object {$_.name -eq "MaxBackupIndex"} | Select-Object -ExpandProperty value    
-$levelValue = $config.configuration.root.level.value    
+# XML-Konfigurationsdatei laden  
+[xml]$config = Get-Content "C:\Program Files\Siemens\LMS\bin\lmslogcfg.xml"  
+  
+# Appender mit dem Namen "ALL-OUT" auswählen  
+$appender = $config.configuration.appender | Where-Object {$_.name -eq "ALL-OUT"}  
+  
+# Wert der Datei-Parameter des Appenders auswählen  
+$fileValue = $appender.param | Where-Object {$_.name -eq "File"} | Select-Object -ExpandProperty value  
+  
+# Wert des Level-Parameters der Root-Kategorie auswählen  
+$levelValue = $config.configuration.root.level.value  
+  
+# Prüfen, ob der Appender gefunden wurde  
 if ($appender -eq $null) {    
     Write-Output "ERROR: Appender not found."    
 } else {    
+    # Dateipfad und Dateiname aus dem Datei-Parameter extrahieren  
     $filePath = Split-Path $fileValue  
+    $fileName = Split-Path $fileValue -Leaf  
+      
+    # Wert von MaxBackupIndex auswählen  
+    $maxBackupIndex = $appender.param | Where-Object {$_.name -eq "MaxBackupIndex"} | Select-Object -ExpandProperty value  
+      
+    # Ausgabe der Ergebnisse  
     Write-Output "File Value: $fileValue"    
     Write-Output "File Path: $filePath"  
+    Write-Output "File Name: $fileName"  
     Write-Output "Max Backup Index: $maxBackupIndex"  
     Write-Output "Level Value: $levelValue"    
-    Write-Output "Appender: $($appender.InnerXml)"    
+    #Write-Output "Appender: $($appender.InnerXml)"    
 }  
