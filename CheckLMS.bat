65029c95b8caf2598d9cd5f21304f63503506efb
Christian Imfeld <christian.imfeld@siemens.com>
2023-10-30 15:30:59 +0100

    rem     30-Oct-2023: rem        - Re-add: https://ipinfo.io/org  (Fix: Defect 2376762)

--- CheckLMS.bat
+++ CheckLMS.bat
@@ -103,6 +103,8 @@ rem        - Change IP check from http to https (Fix: Defect 2376762)
 rem        - Remove check against: http://www.ifconfig.io/  (Fix: Defect 2376762)
 rem        - Remove check against: https://ipinfo.io/org  (Fix: Defect 2376762)
 rem        - add further URLs: https://ip6.me/api/ and https://ip6only.me/api/
+rem     30-Oct-2023:
+rem        - Re-add: https://ipinfo.io/org  (Fix: Defect 2376762)
 rem
 rem     SCRIPT USAGE:
 rem        - Call script w/o any parameter is the default and collects relevant system information.
@@ -139,8 +141,8 @@ rem          Debug Options:
 rem              - /goto <gotolabel>            jump to a dedicated part within script.
 rem  
 rem
-set LMS_SCRIPT_VERSION="CheckLMS Script 27-Oct-2023"
-set LMS_SCRIPT_BUILD=20231027
+set LMS_SCRIPT_VERSION="CheckLMS Script 30-Oct-2023"
+set LMS_SCRIPT_BUILD=20231030
 set LMS_SCRIPT_PRODUCTID=6cf968fa-ffad-4593-9ecb-7a6f3ea07501
 
 rem https://stackoverflow.com/questions/15815719/how-do-i-get-the-drive-letter-a-batch-script-is-running-from
@@ -3136,7 +3138,7 @@ if not defined LMS_SKIPWINDOWS (
 	)
 	echo ---------------- Retrieve public IP address: from https://api.ipify.org                   >> !REPORT_LOGFILE! 2>&1
 	echo Start at !DATE! !TIME! .... Connection Test, check https://api.ipify.org                  >> !REPORT_LOGFILE! 2>&1
-	powershell -Command "(New-Object Net.WebClient).DownloadFile('https://api.ipify.org', '!CHECKLMS_REPORT_LOG_PATH!\connection_test_ipify.txt')" >!CHECKLMS_REPORT_LOG_PATH!\connection_test_ipify_log.txt 2>&1
+	powershell -Command "(New-Object Net.WebClient).DownloadFile('https://api.ipify.org', '!CHECKLMS_REPORT_LOG_PATH!\connection_test_ipify.txt')" >!CHECKLMS_REPORT_LOG_PATH!\connection_test_ipify.log 2>&1
 	if !ERRORLEVEL!==0 (
 		rem Connection Test: PASSED
 		echo     Connection Test PASSED, can access https://api.ipify.org
@@ -3147,7 +3149,21 @@ if not defined LMS_SKIPWINDOWS (
 		rem Connection Test: FAILED
 		echo     Connection Test FAILED, cannot access https://api.ipify.org
 		echo Connection Test FAILED, cannot access https://api.ipify.org                           >> !REPORT_LOGFILE! 2>&1
-		type !CHECKLMS_REPORT_LOG_PATH!\connection_test_ipify_log.txt                              >> !REPORT_LOGFILE! 2>&1
+		type !CHECKLMS_REPORT_LOG_PATH!\connection_test_ipify.log                                  >> !REPORT_LOGFILE! 2>&1
+	)
+	echo ---------------- Retrieve ISP name: from https://ipinfo.io/org                            >> !REPORT_LOGFILE! 2>&1
+	echo Start at !DATE! !TIME! .... Connection Test, check https://ipinfo.io/org                  >> !REPORT_LOGFILE! 2>&1
+	powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipinfo.io/org', '!CHECKLMS_REPORT_LOG_PATH!\connection_test_ipinfo.txt')" >!CHECKLMS_REPORT_LOG_PATH!\connection_test_ipinfo.log 2>&1
+	if !ERRORLEVEL!==0 (
+		rem Connection Test: PASSED
+		echo     Connection Test PASSED, can access https://ipinfo.io/org
+		echo Connection Test PASSED, can access https://ipinfo.io/org                              >> !REPORT_LOGFILE! 2>&1
+		type !CHECKLMS_REPORT_LOG_PATH!\connection_test_ipinfo.txt                                 >> !REPORT_LOGFILE! 2>&1
+	) else if !ERRORLEVEL!==1 (
+		rem Connection Test: FAILED
+		echo     Connection Test FAILED, cannot access https://ipinfo.io/org
+		echo Connection Test FAILED, cannot access https://ipinfo.io/org                           >> !REPORT_LOGFILE! 2>&1
+		echo     Details, see '!CHECKLMS_REPORT_LOG_PATH!\connection_test_ipinfo.log'              >> !REPORT_LOGFILE! 2>&1
 	)
 	echo Start at !DATE! !TIME! ....                                                               >> !REPORT_LOGFILE! 2>&1
 ) else (
